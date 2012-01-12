f545349d7aa30bceb672ce390a7db5b74e387769
pima <pima@3eclipses.com>
2012-01-12 20:43:44 +0100

    added constants to .ec1.ini.ering.data.rb

--- ub10.04_x86_64/erings/cluster.core/ini_ering_templates/variation_01/.ec1.ini.ering/.ec1.ini.ering.data.rb
+++ ub10.04_x86_64/erings/cluster.core/ini_ering_templates/variation_01/.ec1.ini.ering/.ec1.ini.ering.data.rb
@@ -1,11 +1,16 @@
-
 module Ec1 module Supervision module Clusters
 
-bou = "dou"
-
-
-
+EC1_MACHINE_SSH_PORT = ""
+EC1_ENTITY_DOMAIN = ""
+EC1_MACHINE_HOSTNAME = ""
+EC1_MAINUSER_PASSWORD = ""
+EC1_MAINUSER_UID = ""
+EC1_USER_NAME = ""
+EC1_MAINUSER_GID = ""
+EC1_MAINUSER_EMAIL = ""
+EC1_MAINUSER_AUTHORIZED_KEYS = ""
+EC1_ROOT_PASSWORD = ""
+EC1_ROOT_NAME = ""
 
 end end end
-
 
